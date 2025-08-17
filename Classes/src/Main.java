9a4df7294b12a5aa285c9364ace73a681c40d5cf
Renan Fresatto <renan.martins@germinare.org.br>
2025-08-17 14:41:02 -0300

    initial commit

--- Classes/src/Main.java
+++ Classes/src/Main.java
@@ -4,10 +4,7 @@ import static java.lang.System.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+    String name;
+    int idade;
     }
 }
