d2345b7a7cf76e24e9cb73e21c5d426a085ac5e1
Renan Fresatto <renan.fresatto98@gmail.com>
2025-09-04 16:32:28 -0300

    Revert "ajuste de nome da pasta raiz"

---
+++ Classes/src/br/com/fhai/model/usuarios/contasBancaria/ContasBancaria.java
@@ -0,0 +1,94 @@
+package br.com.fhai.model.usuarios.contasBancaria;
+
+import br.com.fhai.model.usuarios.contasBancaria.cartoes.Cartoes;
+
+public class ContasBancaria {
+    private String nome;
+    private String numeroConta;
+    private String agencia;
+    private String tipoConta; // "corrente", "poupanca", "salario"
+    private double saldo = 0;
+    private Cartoes cartao;
+
+    //  Contrutores
+    public ContasBancaria() {
+    }
+
+    public ContasBancaria(String nome, String numeroConta, String agencia, String tipoConta) {
+        this.nome = nome;
+        this.numeroConta = numeroConta;
+        this.agencia = agencia;
+        this.tipoConta = tipoConta;
+    }
+
+    public ContasBancaria(String banco, String numeroConta, String agencia, String tipoConta, double saldo, Cartoes cartao) {
+        this.nome = banco;
+        this.numeroConta = numeroConta;
+        this.agencia = agencia;
+        this.tipoConta = tipoConta;
+        this.saldo = saldo;
+        this.cartao = cartao;
+    }
+
+    //  Getters and Setters ... Atalho para criação padrão(IDE: Intellij): Alt + Ins
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getTipoConta() {
+        return tipoConta;
+    }
+
+    public void setTipoConta(String tipoConta) {
+        this.tipoConta = tipoConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Cartoes getCartao() {
+        return cartao;
+    }
+
+    public void setCartao(Cartoes cartao) {
+        this.cartao = cartao;
+    }
+
+    //  Métodos
+    public String exibirDados() {
+        return "Banco:" + this.nome +
+                "\nNumero da conta: " + this.numeroConta +
+                "\nSaldo: R$ " + this.cartao.getSaldo();
+    }
+
+    public String exibirDadosCartão() {
+        return "-~-~-~-~-~- Dados do Cartão -~-~-~-~-~-" +
+                this.getCartao().exibirDados();
+
+    }
+}
