b6cf89bc51cde4916621e3dd5f929e1360839c83
Renan Fresatto <renan.martins@germinare.org.br>
2025-11-04 01:28:30 -0300

    adicionado validações de login e token jwt

--- sistema-fhai-springboot-main/src/main/java/br/com/fiap/fhai/autenticacao/controller/AutenticacaoController.java
+++ sistema-fhai-springboot-main/src/main/java/br/com/fiap/fhai/autenticacao/controller/AutenticacaoController.java
@@ -1,4 +1,61 @@
 package br.com.fiap.fhai.autenticacao.controller;
 
+import br.com.fiap.fhai.autenticacao.model.LoginRequest;
+import br.com.fiap.fhai.autenticacao.model.UsuarioResponse;
+import br.com.fiap.fhai.autenticacao.service.AutenticacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(
+        origins = "http://localhost:3000",
+        allowedHeaders = "*",
+        allowCredentials = "true"
+)
 public class AutenticacaoController {
+
+    @Autowired
+    private AutenticacaoService autenticacaoService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        try {
+            UsuarioResponse usuario = autenticacaoService.autenticar(loginRequest);
+            return ResponseEntity.ok(usuario);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new ErrorResponse(e.getMessage()));
+        }
+    }
+
+    @GetMapping("/validar")
+    public ResponseEntity<?> validarToken(@RequestHeader("Authorization") String authHeader) {
+        try {
+            String token = authHeader.replace("Bearer ", "");
+            UsuarioResponse usuario = autenticacaoService.obterUsuarioPorToken(token);
+            return ResponseEntity.ok(usuario);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new ErrorResponse(e.getMessage()));
+        }
+    }
+
+    static class ErrorResponse {
+        private String mensagem;
+
+        public ErrorResponse(String mensagem) {
+            this.mensagem = mensagem;
+        }
+
+        public String getMensagem() {
+            return mensagem;
+        }
+
+        public void setMensagem(String mensagem) {
+            this.mensagem = mensagem;
+        }
+    }
 }
--- sistema-fhai-springboot-main/src/main/java/br/com/fiap/fhai/autenticacao/model/UsuarioResponse.java
+++ sistema-fhai-springboot-main/src/main/java/br/com/fiap/fhai/autenticacao/model/UsuarioResponse.java
@@ -1,4 +1,40 @@
 package br.com.fiap.fhai.autenticacao.model;
 
 public class UsuarioResponse {
+    private int id;
+    private String nome;
+    private String token; // Novo campo
+
+    public UsuarioResponse() {}
+
+    public UsuarioResponse(int id, String nome, String token) {
+        this.id = id;
+        this.nome = nome;
+        this.token = token;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
